de4b9a12b856f6726e0ac16ffdfa3b9b0944555a
Zilin Ye <zilin.ye1@ucalgary.ca>
2019-11-01 20:25:58 -0600

    Refactored the class name for Picker

---
+++ camcat/camcat/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  camcat
+//
+//  Created by Zilin Ye on 2019-11-01.
+//  Copyright © 2019 Sophia Zhu. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- camcat/camcat/PickerViewController.swift
+++ camcat/camcat/PickerViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
+class PickerViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
 
     @IBOutlet var imgPreview: UIImageView!
     @IBOutlet var calculateButton: UIButton!
