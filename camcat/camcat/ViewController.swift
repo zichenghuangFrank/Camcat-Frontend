63acc180881cefc4e1244c3083b1ff94132d9e22
sophia <sohiafore@gamil.com>
2019-11-01 18:17:41 -0600

    ,./

--- camcat/camcat/ViewController.swift
+++ camcat/camcat/ViewController.swift
@@ -11,25 +11,26 @@ import UIKit
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
 
     @IBOutlet var photo: UIImageView!
-    @IBOutlet var takePhotoButton: UIButton!
+//    @IBOutlet var takePhotoButton: UIButton!
+    @IBOutlet var calculateButton: UIButton!
     
     
-    @IBAction func takePhoto(_ sender: Any) {
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera){
-            print("inside")
-            let imagePicker = UIImagePickerController()
-            imagePicker.delegate = self
-            imagePicker.sourceType = UIImagePickerController.SourceType.camera
-            imagePicker.allowsEditing = false
-            present(imagePicker, animated: true, completion: nil)
-        }
-        print("out")
-    }
+//    @IBAction func takePhoto(_ sender: Any) {
+//        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera){
+//            print("inside")
+//            let imagePicker = UIImagePickerController()
+//            imagePicker.delegate = self
+//            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+//            imagePicker.allowsEditing = false
+//            present(imagePicker, animated: true, completion: nil)
+//        }
+//        print("out")
+//    }
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        self.dismiss(animated: true, completion: nil)
-        photo.image = info[.originalImage] as? UIImage
-    }
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//        self.dismiss(animated: true, completion: nil)
+//        photo.image = info[.originalImage] as? UIImage
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,9 +38,11 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     }
     
     func setUp(){
-        takePhotoButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
-        takePhotoButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        takePhotoButton.layer.cornerRadius = takePhotoButton.frame.size.width/2
+//        takePhotoButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+//        takePhotoButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+//        takePhotoButton.layer.cornerRadius = takePhotoButton.frame.size.width/2
+        calculateButton.layer.borderWidth = 2
+        calculateButton.layer.cornerRadius = 10
     }
 }
 
