068e2403e702779adcc7b11388bb4357ee9eb7db
Shiwei Sun <shiwei.sun1@ucalgary.ca>
2019-11-02 19:19:05 -0600

    0. Add(Almost finished?) Expression Label; 1.Perfect part of GestureRecog

--- camcat/camcat/MainViewController.swift
+++ camcat/camcat/MainViewController.swift
@@ -13,6 +13,9 @@ class MainViewController: UIViewController {
     var imgData:UIImage!
     var imgView:UIImageView!
     
+    var expressionBar:UILabel!
+    var operatorBarItem:[UIButton]! //Order: 0.Plus, 1.Minus, 2.Multiply, 3.Divide, 4.Undo
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         prepareView()
@@ -23,6 +26,9 @@ class MainViewController: UIViewController {
         let pickedImage = PickedImage.instance.get()
         imgData = pickedImage.data
         drawImageView()
+        
+        drawOperatorBar()
+        drawExpressionBar()
     }
     
     func drawImageView(){
@@ -38,7 +44,14 @@ class MainViewController: UIViewController {
     }
     
     func drawExpressionBar(){
-        
+        expressionBar = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.size.width-CGFloat(50), height: 20))
+        expressionBar.center = CGPoint(x: view.frame.size.width/2, y: 50)
+        expressionBar.layer.cornerRadius=10;
+        expressionBar.layer.borderWidth=1;
+        expressionBar.layer.borderColor=UIColor.black.cgColor
+        expressionBar.textAlignment = .center
+        expressionBar.text = "Equal: 0"
+        self.view.addSubview(expressionBar)
     }
     
     func prepareGestureRecog(){
@@ -46,20 +59,22 @@ class MainViewController: UIViewController {
         imgView.isUserInteractionEnabled = true
         let pinchMethod = UIPinchGestureRecognizer(target: self, action: #selector(pinchImage(sender:)))    //Zoom in/out
         let panMethod = UIPanGestureRecognizer(target: self, action: #selector(handlePan(sender:)))         //Move img with two fingers
-        panMethod.minimumNumberOfTouches = 2
+        let tapMethod = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))         //Tap to select boxes
+        panMethod.minimumNumberOfTouches = 2       //Two finger! Not one
         imgView.addGestureRecognizer(pinchMethod)
         imgView.addGestureRecognizer(panMethod)
-        //
-        let edgePan = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(screenEdgeSwiped))
+        imgView.addGestureRecognizer(tapMethod)
+
+        let edgePan = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(screenEdgeSwiped))   //Swipe Screenedge to pop Mainview
         edgePan.edges = .left
         view.addGestureRecognizer(edgePan)
-        //
     }
     
     //---Gesture Recognition Methods Start---
     @objc func pinchImage(sender: UIPinchGestureRecognizer) {                                                //Zoom in / out
         if let scale = (sender.view?.transform.scaledBy(x: sender.scale, y: sender.scale)) {
             print("Pinch Detected")
+            print("The size of imgView is \(imgView.frame.size)")
             guard scale.a > 1.0 else {return}
             guard scale.d > 1.0 else {return}
             sender.view?.transform = scale
@@ -67,8 +82,10 @@ class MainViewController: UIViewController {
         }
     }
     
-    @objc func handlePan(sender: UIPanGestureRecognizer) {                                                     //Move img with two fingers
+    @objc func handlePan(sender: UIPanGestureRecognizer) {                                                     //Move image with two fingers
         print("2-finger Pan Detedted")
+        print("The center of imgView is \(imgView.center)")
+        print("The coordinator of imgView is \(imgView.frame.origin)")
         let gview = sender.view
         if sender.state == .began || sender.state == .changed {
             let translation = sender.translation(in: gview?.superview)
@@ -77,8 +94,16 @@ class MainViewController: UIViewController {
         }
     }
     
+    @objc func handleTap(sender: UIPanGestureRecognizer) {                                                     //Tap to select boxes
+        guard sender.view != nil else { return }
+        if sender.state == .ended {
+            print("Tap Detected")
+            print("You are tapping \(sender.location(in: self.view))")
+        }
+    }
+    
     @objc func screenEdgeSwiped(_ recognizer: UIScreenEdgePanGestureRecognizer) {
-        print("Screen-edge -swipe Detected")
+        print("Screenedge-swipe Detected")
         if recognizer.state == .recognized {
             self.navigationController?.popViewController(animated: true) //Pop current page
         }
@@ -86,9 +111,6 @@ class MainViewController: UIViewController {
     
     //---Gesture Recognition Methods End---
 
-    func drawImage(){   //Draw blue rectangle on
-        
-    }
     
     /*
     // MARK: - Navigation
